478b600a2751bca0d01a765b65132ff32b5f3745
ymzg <156402499+ymzGu@users.noreply.github.com>
2024-04-07 14:55:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,36 +14,43 @@ function makeSound(key){
     switch(key){ 
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.preload = "auto";
             tom1.play();// .play()不要忘记
             break;
          
         case "a":
             var tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.preload = "auto";
             tom2.play();
             break;
 
         case "s":
             var tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.preload = "auto";
             tom3.play();
             break; 
 
         case "d":
             var tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.preload = "auto";
             tom4.play();
             break;
              
         case "j":
             var tom5 = new Audio("sounds/crash.mp3");
+          tom5.preload = "auto";
             tom5.play();
             break;
                 
         case "k":
             var tom6 = new Audio("sounds/kick-bass.mp3");
+          tom6.preload = "auto";
             tom6.play();
             break;    
             
         case "l":
             var tom7 = new Audio("sounds/snare.mp3");
+          tom7.preload = "auto";
             tom7.play();
             break; 
         
